9a340e7710e1134d762083055d271413bda6f7b0
Thomas Chaplin <tomrchaplin@gmail.com>
2019-03-24 21:03:13 +0000

    Finished fractal tree generator page

--- fractal_trees/js/rand_tree.js
+++ fractal_trees/js/rand_tree.js
@@ -1,10 +1,14 @@
 var strokeSlider;
 var angleSlider;
 var strokeOpacSlider;
+var transitionSlider;
+var minLengthSlider;
+var lenRatioSlider;
 var strokeOpacRatio = 0.95;
 var strokeRatio = 0.6;
 var lenRatio = 0.67;
 var angleRange = 0;
+var minLength = 10;
 var transitionLevel = 6;
 var backCol = 255;
 var canvasSize = 400;
@@ -18,13 +22,18 @@ function setup() {
 	strokeOpacSlider.parent('strokeOpacSlider');
 	strokeSlider = createSlider(0,1,strokeRatio,0.01);
 	strokeSlider.parent('strokeSlider');
+	lenRatioSlider = createSlider(0,0.99,lenRatio,0.01);
+	lenRatioSlider.parent('lenRatioSlider');
 	angleSlider = createSlider(0,PI,PI/2,0.01);
 	angleSlider.parent('angleSlider');
+	transitionSlider = createSlider(0,10,6,1);
+	transitionSlider.parent('transitionSlider');
+	minLengthSlider = createSlider(1,20,minLength,1);
+	minLengthSlider.parent('minLengthSlider');
 	button = createButton('Draw tree');
 	button.parent('create_button');
 	button.mousePressed(drawTree);
 	button.addClass('std_button');
-	stroke('green');
 }
 
 function windowResized(){
@@ -39,6 +48,13 @@ function draw() {
 	document.getElementById('strokeReadout').innerHTML = strokeRatio;
 	angleRange = angleSlider.value();
 	document.getElementById('angleReadout').innerHTML = angleRange;
+	transitionLevel = transitionSlider.value();
+	document.getElementById('transitionReadout').innerHTML = transitionLevel;
+	minLength = minLengthSlider.value();
+	document.getElementById('minLengthReadout').innerHTML = minLength;
+	lenRatio = lenRatioSlider.value();
+	document.getElementById('lenRatioReadout').innerHTML = lenRatio;
+	backCol = "#" + document.getElementById("background_colour").value;
 }
 
 function drawTree(){
@@ -52,7 +68,7 @@ function drawBranch(len, level) {
 	stroke(getColour(level));
 	line(0,0,0,-len);
 	translate(0,-len);
-	if(len < 4) {
+	if(len < minLength) {
 		return;
 	}
 	var numBranches;
@@ -72,11 +88,23 @@ function drawBranch(len, level) {
 	}
 }
 
+// Thanks to https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb 
+function hexToRGB(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
+
 function getColour(level) {
 	var opacity = Math.pow(strokeOpacRatio, level - 1);
 	if (level < transitionLevel) {
-		return 'rgba(76, 66, 55, ' + opacity + ')';
+		var c = hexToRGB(document.getElementById("lower_colour").value);
+		return 'rgba(' + c.r + "," + c.g + "," + c.b + "," + opacity + ")";
 	} else {
-		return 'rgba(143, 226, 47, ' + opacity + ')';
+		var c = hexToRGB(document.getElementById("upper_colour").value);
+		return 'rgba(' + c.r + "," + c.g + "," + c.b + "," + opacity + ")";
 	}
 }
